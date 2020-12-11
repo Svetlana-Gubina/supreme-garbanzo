814f648c06bf6bea66aeb148c21b49c9d42bd186
Svetlana Terenteva <gubinas83@gmail.com>
2020-12-11 20:03:44 +0300

    feat: about, breed pages - mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+'use strict';
+(function(){
+const BREAKPOINTS = {
+  tablet: '768px'
+};
+
+const toggle = document.querySelector(".main-nav__toggle");
+const menu = document.querySelector(".main-nav__list");
+
+const handleBreackpointChange = function (event) {
+  if (event.matches) {
+    menu.classList.remove("menu-close");
+  } else {
+    menu.classList.add("menu-close");
+  }
+}
+
+const modalMenuActive = function() {
+   toggle.classList.remove("visually-hidden");
+};
+
+const mediaQueryList = window.matchMedia('(min-width:' + BREAKPOINTS.tablet.toString() + '');
+mediaQueryList.addEventListener("change", handleBreackpointChange);
+handleBreackpointChange(mediaQueryList);
+
+window.addEventListener("load", modalMenuActive);
+
+toggle.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  this.classList.toggle("main-nav__toggle-close");
+  menu.classList.toggle("menu-close");
+});
+})();
